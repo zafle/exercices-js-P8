883b431c5892a735a9ab2db8c075aa255c8f81b7
zafle <fleutotanne@free.fr>
2024-08-07 16:32:26 +0200

    exercice 2

--- Exercice2/index.js
+++ Exercice2/index.js
@@ -1,5 +1,26 @@
 // Créez votre fonction ici
+function calculate(a, b, operator) {
 
+    let nb1 = parseInt(a)
+    let nb2 = parseInt(b)
+
+    switch (operator) {
+        case "+" :
+            return parseInt(nb1+nb2)
+        case "-" :
+            return parseInt(nb1-nb2)
+        case "*" :
+            return parseInt(nb1*nb2)
+        case "/" :
+            if (nb2 === 0) {
+                return "Division by zero is not allowed"
+            } else {
+                return parseInt(nb1/nb2)
+            }
+        default :
+            return "Invalid operator"
+    }
+}
 // Exemples d'utilisation de la fonction
 console.log(calculate(5, 3, '+'));   // Affiche 8
 console.log(calculate(10, 4, '-'));  // Affiche 6
