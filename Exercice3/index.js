c7d5d68d864fb092cefd9a928d47e5c7d8960fac
zafle <fleutotanne@free.fr>
2024-08-07 16:45:30 +0200

    exercice 3

--- Exercice3/index.js
+++ Exercice3/index.js
@@ -1,5 +1,22 @@
 // Créez votre fonction ici
 
+function calculateAverage(array) {
+
+    if ( array && array.length) {
+
+        let minimum = Math.min(...array)
+        let maximum = Math.max(...array)
+
+        let average = (minimum + maximum) / 2
+
+        return average
+
+    } else {
+
+        return "No numbers to calculate average"
+    }
+}
+
 // Exemples d'utilisation de la fonction
 console.log(calculateAverage([5, 10, 15])) // retourne 10
 console.log(calculateAverage([10, 20, 30, 20])) // retourne 20
