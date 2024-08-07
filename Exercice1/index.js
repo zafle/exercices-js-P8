a05f5675cbaaffe676a7f9e81fd7e8233789420b
zafle <fleutotanne@free.fr>
2024-08-07 15:59:35 +0200

    exercice 1

--- Exercice1/index.js
+++ Exercice1/index.js
@@ -1,3 +1,17 @@
 //votre code ici
 
+function pairNumbers(start, end) {
+
+    let result = []
+
+    for (let i = start; i <= end; i++) {
+
+        if ( i % 2 === 0) {
+            result.push(i)
+        }
+    }
+
+    return result.toString()
+}
+
 export default pairNumbers
